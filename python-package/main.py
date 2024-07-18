57f60def45df653eae6420bc6518c974e57baa7e
hphan0812 <honghanh.phan@nextrobotics.io>
2024-07-18 14:32:19 +0700

    [FEAT] hphan: upload file img of company

--- python-package/main.py
+++ python-package/main.py
@@ -1,6 +1,4 @@
 import cv2
-import numpy as np
-import insightface
 from insightface.app import FaceAnalysis
 from insightface.data import get_image as ins_get_image
 
@@ -9,4 +7,4 @@ app.prepare(ctx_id=0, det_size=(640, 640))
 img = ins_get_image('Minh')
 faces = app.get(img)
 rimg = app.draw_on(img, faces)
-cv2.imwrite("./Minh_output.jpg", rimg)
+cv2.imwrite(f"/disk/sdc/hanh/model_face_reg/python-package/out_face/face_1.png", rimg)
